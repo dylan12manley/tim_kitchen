32167c44fcb7f94fd59ca739a7e4bb4de6464541
Dylan Manley <dylan.manley@gmail.com>
2020-01-24 12:00:22 -0800

    undo devise attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # devise_for :views
-  devise_for :users
-  # root to: 'products#index'
+  root to: 'products#index'
   resources :products do
     resources :reviews
   end
