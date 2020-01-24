769a17aa7c3fe0f35d473df18b41495ff50aff7d
Dylan Manley <dylan.manley@gmail.com>
2020-01-24 15:47:08 -0800

    adjust users table so admin boolean defalts to false

--- db/migrate/20200124200215_add_users.rb
+++ db/migrate/20200124200215_add_users.rb
@@ -1,11 +1,10 @@
 class AddUsers < ActiveRecord::Migration[6.0]
   def change
-    # drop_table :users
-    create_table :users do |t|
-      t.column :email, :string
-      t.column :admin, :boolean
-      t.column :password_hash, :string
-      t.column :password_salt, :string
+    # create_table :users do |t|
+    #   t.column :email, :string
+    #   t.column :admin, :boolean default: false
+    #   t.column :password_hash, :string
+    #   t.column :password_salt, :string
     end
   end
 end
