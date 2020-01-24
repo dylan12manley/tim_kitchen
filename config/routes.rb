88893871e2092843129f544feabbbd30fac4d8cb
Dylan Manley <dylan.manley@gmail.com>
2020-01-24 11:13:02 -0800

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'products#index'
+  root to: 'pages#home'
+  devise_for :views
+  devise_for :users
+  # root to: 'products#index'
   resources :products do
     resources :reviews
   end
