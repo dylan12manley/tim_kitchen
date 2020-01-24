2e8c706933d884f887f6f983eb53843a12184159
Dylan Manley <dylan.manley@gmail.com>
2020-01-24 10:38:40 -0800

    adjust intergration test so they pass

--- spec/models/kitchen_intergration_spec.rb
+++ spec/models/kitchen_intergration_spec.rb
@@ -4,11 +4,11 @@ describe('create a product path', {:type => :feature}) do
   it('creates a product and goes to that product page') do
     visit('/products')
     click_on('Create new product')
-    fill_in('Name', :with => 'tim')
+    fill_in('Name', :with => 'dehhillnuh')
     fill_in('Cost', :with => 2)
     fill_in('Country of origin', :with => "USA")
     click_on('Create Product')
-    expect(page).to have_content('Tim')
+    expect(page).to have_content('Dehhillnuh')
   end
 end
 
@@ -16,11 +16,11 @@ describe('clears all product paths', {:type => :feature}) do
   it('creates a product and deleats that product') do
     visit('/products')
     click_on('Create new product')
-    fill_in('Name', :with => 'tim')
+    fill_in('Name', :with => 'dehhillnuh')
     fill_in('Cost', :with => 2)
     fill_in('Country of origin', :with => "USA")
     click_on('Create Product')
-    click_on('Tim')
+    click_on('Dehhillnuh')
     click_on('Delete')
     # click_on('Ok')
     expect(page).to have_content("Product successfully deleted.")
@@ -31,13 +31,13 @@ describe('create a review path', {:type => :feature}) do
   it('creates a product with a review and then goes to the product page') do
     visit('/products')
     click_on('Create new product')
-    fill_in('Name', :with => 'tim')
+    fill_in('Name', :with => 'dehhillnuh')
     fill_in('Cost', :with => 2)
     fill_in('Country of origin', :with => "USA")
     click_on('Create Product')
-    click_on('Tim')
+    click_on('Dehhillnuh')
     click_on('Add an review')
-    fill_in('Author', :with => 'timmy')
+    fill_in('Author', :with => 'dehhillnuhmy')
     fill_in('Content body', :with => 'this is the content body this is the content body this is the content body this is the content body this is the content body this is the content body this is the content body this is the content body')
     fill_in('Rating', :with => "4")
     click_on('Create Review')
