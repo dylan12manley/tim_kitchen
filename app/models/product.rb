a2e36a422db30eb5ae9e2169a270be6caff10c75
Dylan Manley <dylan.manley@gmail.com>
2020-01-24 17:28:00 -0800

    add tests for the product class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  
+
   before_save(:titleize_name)
 
   validates :name, presence: true
   validates :cost, presence: true
   validates :country_of_origin, presence: true
+  validates_numericality_of :cost, greater_than: 0
 
   scope :most_reviews, -> {(
     select("products.id, products.name, count(review.id) as reviews_count")
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 describe Product do
+  context '(associations)' do
   it { should have_many(:reviews) }
+end
+context '(validations)' do
+  it { should validate_presence_of(:name)}
+  it { should validate_presence_of(:cost)}
+  it { should validate_presence_of(:country_of_origin)}
+end
+context '(scopes)' do
+  it '.most_recent returns the 3 most recent products' do
+    product_one = Product.create(name: 'apple', cost: 1, country_of_origin: 'USA')
+    product_two = Product.create(name: 'orange', cost: 1, country_of_origin: 'USA')
+    product_three = Product.create(name: 'banana', cost: 1, country_of_origin: 'USA')
+    product_four = Product.create(name: 'candy', cost: 1, country_of_origin: 'Canada')
+    expect(Product.most_recent).to eq [product_four, product_three, product_two]
+  end
+  it '.local_products returns all products with USA as country of origin' do
+    product_one = Product.create(name: 'apple', cost: 1, country_of_origin: 'USA')
+    product_two = Product.create(name: 'orange', cost: 1, country_of_origin: 'USA')
+    product_three = Product.create(name: 'banana', cost: 1, country_of_origin: 'USA')
+    product_four = Product.create(name: 'candy', cost: 1, country_of_origin: 'Canada')
+    expect(Product.local_products).to eq [ product_one, product_two, product_three]
+  end
+end
   it("titleizes the name of an product") do
     product = Product.create({name: "cripps Pink lady", cost: 1.50, country_of_origin: "Canada"})
     expect(product.name()).to(eq("Cripps Pink Lady"))
